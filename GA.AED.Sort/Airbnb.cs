901b951290bde3908c3c7327e0d0047835a53bef
Daniel Braz <drz.braz@gmail.com>
2020-03-14 17:23:26 -0300

    Selection Sort

--- GA.AED.Sort/Airbnb.cs
+++ GA.AED.Sort/Airbnb.cs
@@ -1,103 +1,138 @@
-﻿namespace GA.AED.Sort {
-    public class Airbnb {
+﻿namespace GA.AED.Sort
+{
+  public class Airbnb
+  {
 
-        #region [ Properties ]
+    #region [ Properties ]
 
-        public int RoomId { get; set; }
-        public int HostId { get; set; }
-        public string RoomType { get; set; }
-        public string Country { get; set; }
-        public string City { get; set; }
-        public string Neighborhood { get; set; }
-        public double Reviews { get; set; }
-        public double OverallSatisfaction { get; set; }
-        public double Accommodates { get; set; }
-        public double Bedrooms { get; set; }
-        public double Price { get; set; }
-        public string PropertyType { get; set; }
+    public int RoomId { get; set; }
+    public int HostId { get; set; }
+    public string RoomType { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string Neighborhood { get; set; }
+    public double Reviews { get; set; }
+    public double OverallSatisfaction { get; set; }
+    public double Accommodates { get; set; }
+    public double Bedrooms { get; set; }
+    public double Price { get; set; }
+    public string PropertyType { get; set; }
 
-        #endregion
+    #endregion
 
-        #region [ Constructor ]
+    #region [ Constructor ]
 
-        public Airbnb(int roomId,
-                      int hostId,
-                      string roomType,
-                      string city,
-                      string country,
-                      string neighborhood,
-                      double review,
-                      double overallSatisfaction,
-                      double accommodates,
-                      double bedrooms,
-                      double price,
-                      string propertyType) {
+    public Airbnb(int roomId,
+                  int hostId,
+                  string roomType,
+                  string city,
+                  string country,
+                  string neighborhood,
+                  double review,
+                  double overallSatisfaction,
+                  double accommodates,
+                  double bedrooms,
+                  double price,
+                  string propertyType)
+    {
 
-            this.RoomId = roomId;
-            this.HostId = hostId;
-            this.RoomType = roomType;
-            this.City = city;
-            this.Country = country;
-            this.Neighborhood = neighborhood;
-            this.Reviews = review;
-            this.OverallSatisfaction = overallSatisfaction;
-            this.Accommodates = accommodates;
-            this.Bedrooms = bedrooms;
-            this.Price = price;
-            this.PropertyType = propertyType;
-        }
-
-        #endregion
-
-        #region [ Copy ]
-
-        public static Airbnb[] GetCopy(Airbnb[] airbnbs, int lenght) {
-            Airbnb[] airbnbCopy = new Airbnb[lenght];
-
-            for (int i = 0; i < lenght; i++) {
-                airbnbCopy[i] = airbnbs[i];
-            }
-
-            return airbnbCopy;
-        }
-
-        #endregion
-
-        #region [ Sort ]
-
-        public static void BubbleSort(Airbnb[] airbnbs) {
-            int length = airbnbs.Length;
-            for (int i = 0; i < length; i++) {
-                for (int j = 0; j < length - i - 1; j++) {
-                    if(airbnbs[j].RoomId > airbnbs[i].RoomId) {
-                        Airbnb aux = airbnbs[j];
-                        airbnbs[j] = airbnbs[j + 1];
-                        airbnbs[j + 1] = aux;
-                    }
-                }
-            }
-        }
-
-        public static void SelectSort(Airbnb[] airbnbs) {
-            //TODO: Implementar o SelectSort
-        }
-
-        public static void InsertSort(Airbnb[] airbnbs) {
-            //TODO: Implementar o InsertSort
-        }
-
-        public static void MergeSort(Airbnb[] airbnbs) {
-            //TODO: Implementar o MergeSort
-        }
-
-        public static void QuickSort(Airbnb[] airbnbs) {
-            //TODO: Implementar o QuickSort
-        }
-
-        public static void CreateSort(Airbnb[] airbnbs) {
-            //TODO: Implementar o CreateSort
-        }
-
-        #endregion
+      this.RoomId = roomId;
+      this.HostId = hostId;
+      this.RoomType = roomType;
+      this.City = city;
+      this.Country = country;
+      this.Neighborhood = neighborhood;
+      this.Reviews = review;
+      this.OverallSatisfaction = overallSatisfaction;
+      this.Accommodates = accommodates;
+      this.Bedrooms = bedrooms;
+      this.Price = price;
+      this.PropertyType = propertyType;
     }
+
+    #endregion
+
+    #region [ Copy ]
+
+    public static Airbnb[] GetCopy(Airbnb[] airbnbs, int lenght)
+    {
+      Airbnb[] airbnbCopy = new Airbnb[lenght];
+
+      for (int i = 0; i < lenght; i++)
+      {
+        airbnbCopy[i] = airbnbs[i];
+      }
+
+      return airbnbCopy;
+    }
+
+    #endregion
+
+    #region [ Sort ]
+
+    public static void BubbleSort(Airbnb[] airbnbs)
+    {
+      int length = airbnbs.Length;
+      for (int i = 0; i < length; i++)
+      {
+        for (int j = 0; j < length - i - 1; j++)
+        {
+          if (airbnbs[j].RoomId > airbnbs[i].RoomId)
+          {
+            Airbnb aux = airbnbs[j];
+            airbnbs[j] = airbnbs[j + 1];
+            airbnbs[j + 1] = aux;
+          }
+        }
+      }
+    }
+
+    public static void SelectSort(Airbnb[] airbnbs)
+    {
+      //TODO: Implementar o SelectSort
+      int min, aux;
+      for (int i = 0; i < airbnbs.Length - 1; i++)
+      {
+        min = i;
+        for (int j = i + 1; j < airbnbs.Length; j++)
+        {
+          if (airbnbs[j].RoomId < airbnbs[min].RoomId)
+          {
+            min = j;
+          }
+        }
+        if (min != i)
+        {
+          aux = airbnbs[min].RoomId;
+          airbnbs[min].RoomId = airbnbs[i].RoomId;
+          airbnbs[i].RoomId = aux;
+        }
+
+
+      }
+
+    }
+
+    public static void InsertSort(Airbnb[] airbnbs)
+    {
+      //TODO: Implementar o InsertSort
+    }
+
+    public static void MergeSort(Airbnb[] airbnbs)
+    {
+      //TODO: Implementar o MergeSort
+    }
+
+    public static void QuickSort(Airbnb[] airbnbs)
+    {
+      //TODO: Implementar o QuickSort
+    }
+
+    public static void CreateSort(Airbnb[] airbnbs)
+    {
+      //TODO: Implementar o CreateSort
+    }
+
+    #endregion
+  }
 }
--- GA.AED.Sort/Program.cs
+++ GA.AED.Sort/Program.cs
@@ -2,253 +2,300 @@
 using System.Diagnostics;
 using System.IO;
 
-namespace GA.AED.Sort {
-    public class Program {
+namespace GA.AED.Sort
+{
+  public class Program
+  {
 
-        #region [ Consts ]
+    #region [ Consts ]
 
-        private const string FILE_READ_PATH_RANDOM = @"Files\dados_airbnb_random.csv";
-        private const string FILE_READ_PATH_DESC = @"Files\dados_airbnb_desc.csv";
-        private const string FILE_READ_PATH_ASC = @"Files\dados_airbnb_asc.csv";
-        private const string FILE_LOG_WRITE_PATH = @"Files\Logs\Log {0}.csv";
-        private const string FILE_CONSOLE_LOG_WRITE_PATH = @"Files\Logs\ConsoleLog {0}.txt";
+    private const string FILE_READ_PATH_RANDOM = @"Files/dados_airbnb_random.csv";
+    private const string FILE_READ_PATH_DESC = @"Files/dados_airbnb_desc.csv";
+    private const string FILE_READ_PATH_ASC = @"Files/dados_airbnb_asc.csv";
+    private const string FILE_LOG_WRITE_PATH = @"Files/Logs/Log {0}.csv";
+    private const string FILE_CONSOLE_LOG_WRITE_PATH = @"Files/Logs/ConsoleLog {0}.txt";
 
-        private const string BUBBLE_SORT = "BubbleSort";
-        private const string SELECT_SORT = "SelectSort";
-        private const string INSERT_SORT = "InsertSort";
-        private const string MERGE_SORT = "MergeSort";
-        private const string QUICK_SORT = "QuickSort";
+    private const string BUBBLE_SORT = "BubbleSort";
+    private const string SELECT_SORT = "SelectSort";
+    private const string INSERT_SORT = "InsertSort";
+    private const string MERGE_SORT = "MergeSort";
+    private const string QUICK_SORT = "QuickSort";
 
-        private const string RANDOM = "Aleatorio";
-        private const string DESC = "Decrescente";
-        private const string ASC = "Crescente";
+    private const string RANDOM = "Aleatorio";
+    private const string DESC = "Decrescente";
+    private const string ASC = "Crescente";
 
-        private const int REPETITIONS_TIME = 5;
-        private const int THOUSAND = 1000;
+    private const int REPETITIONS_TIME = 5;
+    private const int THOUSAND = 1000;
 
-        private static DateTime StartDate = DateTime.Now;
+    private static DateTime StartDate = DateTime.Now;
 
-        #endregion
+    #endregion
 
-        public static void Main(string[] args) {
-            Airbnb[] airbnbsRandom = ReadAirbnbFile(FILE_READ_PATH_RANDOM);
-            Airbnb[] airbnbsDesc = ReadAirbnbFile(FILE_READ_PATH_DESC);
-            Airbnb[] airbnbsAsc = ReadAirbnbFile(FILE_READ_PATH_ASC);
+    public static void Main(string[] args)
+    {
+      Airbnb[] airbnbsRandom = ReadAirbnbFile(FILE_READ_PATH_RANDOM);
+      Airbnb[] airbnbsDesc = ReadAirbnbFile(FILE_READ_PATH_DESC);
+      Airbnb[] airbnbsAsc = ReadAirbnbFile(FILE_READ_PATH_ASC);
 
-            for (int i = 1; i <= 7; i++) {
-                int quantity = Convert.ToInt32(Math.Pow(2, i) * THOUSAND);
+      for (int i = 1; i <= 7; i++)
+      {
+        int quantity = Convert.ToInt32(Math.Pow(2, i) * THOUSAND);
 
-                #region [ BubbleSort ]
+        #region [ BubbleSort ]
 
-                Bubble(airbnbsRandom, RANDOM, quantity);
-                Bubble(airbnbsDesc, DESC, quantity);
-                Bubble(airbnbsAsc, ASC, quantity);
-
-                #endregion
-
-                #region [ SelectSort ]
-
-                Select(airbnbsRandom, RANDOM, quantity);
-                Select(airbnbsDesc, DESC, quantity);
-                Select(airbnbsAsc, ASC, quantity);
-
-                #endregion
-
-                #region [ InsertSort ]
-
-                Insert(airbnbsRandom, RANDOM, quantity);
-                Insert(airbnbsDesc, DESC, quantity);
-                Insert(airbnbsAsc, ASC, quantity);
-
-                #endregion
-
-                #region [ MergeSort ]
-
-                Merge(airbnbsRandom, RANDOM, quantity);
-                Merge(airbnbsDesc, DESC, quantity);
-                Merge(airbnbsAsc, ASC, quantity);
-
-                #endregion
-
-                #region [ QuickSort ]
-
-                Quick(airbnbsRandom, RANDOM, quantity);
-                Quick(airbnbsDesc, DESC, quantity);
-                Quick(airbnbsAsc, ASC, quantity);
-
-                #endregion
-
-                #region [ CreateSort ]
-
-                Create(airbnbsRandom, RANDOM, quantity);
-                Create(airbnbsDesc, DESC, quantity);
-                Create(airbnbsAsc, ASC, quantity);
-
-                #endregion
-            }
-
-        }
-
-        #region [ Sorts ]
-
-        private static void Bubble(Airbnb[] airbnbs, string type, int quantity) {
-            DateTime bubbleSortStart = DateTime.Now;
-
-            double[] bubbleSortTimes = new double[REPETITIONS_TIME];
-            for (int repetition = 0; repetition < REPETITIONS_TIME; repetition++) {
-                Airbnb[] airbnbsBoubbleSort = Airbnb.GetCopy(airbnbs, quantity);
-
-                Stopwatch watch = Stopwatch.StartNew();
-                Airbnb.BubbleSort(airbnbsBoubbleSort);
-                watch.Stop();
-
-                bubbleSortTimes[repetition] = watch.ElapsedMilliseconds / 1000.0;
-            }
-
-            double bubbleAverageTime = AverageTime(bubbleSortTimes);
-            DateTime bubbleSortEnd = DateTime.Now;
-            string consoleLog = $"{quantity,-10} {type,-15} {BUBBLE_SORT,-15} ({bubbleSortStart} - {bubbleSortEnd})";
-
-            Log(BUBBLE_SORT,
-                type,
-                quantity,
-                bubbleAverageTime,
-                Process.GetCurrentProcess().PeakWorkingSet64 / (1024 * 1024));
-
-            ConsoleLog(consoleLog);
-            Console.WriteLine(consoleLog);
-        }
-
-        private static void Select(Airbnb[] airbnbs, string type, int quantity) {
-
-        }
-
-        private static void Insert(Airbnb[] airbnbs, string type, int quantity) {
-
-        }
-
-        private static void Merge(Airbnb[] airbnbs, string type, int quantity) {
-
-        }
-
-        private static void Quick(Airbnb[] airbnbs, string type, int quantity) {
-            DateTime quickSortStart = DateTime.Now;
-
-            double[] quickSortTimes = new double[REPETITIONS_TIME];
-            for (int repetition = 0; repetition < REPETITIONS_TIME; repetition++) {
-                Airbnb[] airbnbsQuickSort = Airbnb.GetCopy(airbnbs, quantity);
-
-                Stopwatch watch = Stopwatch.StartNew();
-                Airbnb.QuickSort(airbnbsQuickSort);
-                watch.Stop();
-
-                quickSortTimes[repetition] = watch.ElapsedMilliseconds / 1000.0;
-            }
-
-            double quickAverageTime = AverageTime(quickSortTimes);
-            DateTime quickSortEnd = DateTime.Now;
-            string consoleLog = $"{quantity,-10} {type,-15} {QUICK_SORT,-15} ({quickSortStart} - {quickSortEnd})";
-
-            Log(QUICK_SORT,
-                type,
-                quantity,
-                quickAverageTime,
-                Process.GetCurrentProcess().PeakWorkingSet64 / (1024 * 1024));
-
-            ConsoleLog(consoleLog);
-            Console.WriteLine(consoleLog);
-        }
-
-        private static void Create(Airbnb[] airbnbs, string type, int quantity) {
-
-        }
+        Bubble(airbnbsRandom, RANDOM, quantity);
+        Bubble(airbnbsDesc, DESC, quantity);
+        Bubble(airbnbsAsc, ASC, quantity);
 
         #endregion
 
-        #region [ Log ]
+        #region [ SelectSort ]
 
-        private static double AverageTime(double[] times) {
-            double max = times[0], min = times[0];
-            double sum = times[0];
-
-            for (int i = 1; i < times.Length; i++) {
-                if (times[i] > max)
-                    max = times[i];
-                else if (times[i] < min)
-                    min = times[i];
-                sum += times[i];
-            }
-
-            return (sum - max - min) / ((times.Length - 2) * 1.0);
-        }
-
-        private static void Log(string description,
-                                string tipo,
-                                int quantity,
-                                double time,
-                                long memory) {
-
-            string path = string.Format(FILE_LOG_WRITE_PATH, StartDate.ToString("dd-MM-yyyy HH-MM"));
-            string linha = string.Empty;
-
-            if (!File.Exists(path)) {
-                File.Create(path).Close();
-                linha += "Algoritmo; Tipo; Quantidade; Tempo; Memoria\n";
-            }
-
-            linha += $"{description}; {tipo}; {quantity}; {time}; {memory}\n";
-            File.AppendAllText(path, linha);
-        }
-
-        private static void ConsoleLog(string consoleLog) {
-            string path = string.Format(FILE_CONSOLE_LOG_WRITE_PATH, StartDate.ToString("dd-MM-yyyy HH-MM"));
-            if (!File.Exists(path)) {
-                File.Create(path).Close();
-            }
-
-            File.AppendAllText(path, consoleLog + "\n");
-        }
+        Select(airbnbsRandom, RANDOM, quantity);
+        Select(airbnbsDesc, DESC, quantity);
+        Select(airbnbsAsc, ASC, quantity);
 
         #endregion
 
-        #region [ Read File ]
+        #region [ InsertSort ]
 
-        private static Airbnb[] ReadAirbnbFile(string path) {
-            string[] lines = File.ReadAllLines(path);
-            Airbnb[] airbnbs = new Airbnb[lines.Length - 1];
-
-            for (int i = 1; i < lines.Length; i++) {
-                string[] line = lines[i].Split(",");
-                int roomId = Convert.ToInt32(line[0]);
-                int hostId = Convert.ToInt32(line[1]);
-                string roomType = line[2];
-                string country = line[3];
-                string city = line[4];
-                string neighborhood = line[5];
-                double review = Convert.ToDouble(line[6]);
-                double overallSatisfaction = Convert.ToDouble(line[7]);
-                double accommodates = Convert.ToDouble(line[8]);
-                double bedrooms = Convert.ToDouble(line[9]);
-                double price = Convert.ToDouble(line[10]);
-                string propertyType = line[11];
-
-                airbnbs[i - 1] = new Airbnb(roomId,
-                                            hostId,
-                                            roomType,
-                                            city,
-                                            country,
-                                            neighborhood,
-                                            review,
-                                            overallSatisfaction,
-                                            accommodates,
-                                            bedrooms,
-                                            price,
-                                            propertyType);
-            }
-
-            return airbnbs;
-        }
+        Insert(airbnbsRandom, RANDOM, quantity);
+        Insert(airbnbsDesc, DESC, quantity);
+        Insert(airbnbsAsc, ASC, quantity);
 
         #endregion
+
+        #region [ MergeSort ]
+
+        Merge(airbnbsRandom, RANDOM, quantity);
+        Merge(airbnbsDesc, DESC, quantity);
+        Merge(airbnbsAsc, ASC, quantity);
+
+        #endregion
+
+        #region [ QuickSort ]
+
+        Quick(airbnbsRandom, RANDOM, quantity);
+        Quick(airbnbsDesc, DESC, quantity);
+        Quick(airbnbsAsc, ASC, quantity);
+
+        #endregion
+
+        #region [ CreateSort ]
+
+        Create(airbnbsRandom, RANDOM, quantity);
+        Create(airbnbsDesc, DESC, quantity);
+        Create(airbnbsAsc, ASC, quantity);
+
+        #endregion
+      }
+
     }
+
+    #region [ Sorts ]
+
+    private static void Bubble(Airbnb[] airbnbs, string type, int quantity)
+    {
+      DateTime bubbleSortStart = DateTime.Now;
+
+      double[] bubbleSortTimes = new double[REPETITIONS_TIME];
+      for (int repetition = 0; repetition < REPETITIONS_TIME; repetition++)
+      {
+        Airbnb[] airbnbsBoubbleSort = Airbnb.GetCopy(airbnbs, quantity);
+
+        Stopwatch watch = Stopwatch.StartNew();
+        Airbnb.BubbleSort(airbnbsBoubbleSort);
+        watch.Stop();
+
+        bubbleSortTimes[repetition] = watch.ElapsedMilliseconds / 1000.0;
+      }
+
+      double bubbleAverageTime = AverageTime(bubbleSortTimes);
+      DateTime bubbleSortEnd = DateTime.Now;
+      string consoleLog = $"{quantity,-10} {type,-15} {BUBBLE_SORT,-15} ({bubbleSortStart} - {bubbleSortEnd})";
+
+      Log(BUBBLE_SORT,
+          type,
+          quantity,
+          bubbleAverageTime,
+          Process.GetCurrentProcess().PeakWorkingSet64 / (1024 * 1024));
+
+      ConsoleLog(consoleLog);
+      Console.WriteLine(consoleLog);
+    }
+
+    private static void Select(Airbnb[] airbnbs, string type, int quantity)
+    {
+
+      DateTime selectionSortStart = DateTime.Now;
+
+      double[] selectionSortTimes = new double[REPETITIONS_TIME];
+      for (int repetition = 0; repetition < REPETITIONS_TIME; repetition++)
+      {
+        Airbnb[] airbnbsSelectionSort = Airbnb.GetCopy(airbnbs, quantity);
+
+        Stopwatch watch = Stopwatch.StartNew();
+        Airbnb.SelectSort(airbnbsSelectionSort);
+        watch.Stop();
+
+        selectionSortTimes[repetition] = watch.ElapsedMilliseconds / 1000.0;
+      }
+
+      double selectionAverageTime = AverageTime(selectionSortTimes);
+      DateTime selectionSortEnd = DateTime.Now;
+      string consoleLog = $"{quantity,-10} {type,-15} {SELECT_SORT,-15} ({selectionSortStart} - {selectionSortEnd})";
+
+      Log(SELECT_SORT,
+          type,
+          quantity,
+          selectionAverageTime,
+          Process.GetCurrentProcess().PeakWorkingSet64 / (1024 * 1024));
+
+      ConsoleLog(consoleLog);
+      Console.WriteLine(consoleLog);
+
+    }
+
+    private static void Insert(Airbnb[] airbnbs, string type, int quantity)
+    {
+
+    }
+
+    private static void Merge(Airbnb[] airbnbs, string type, int quantity)
+    {
+
+    }
+
+    private static void Quick(Airbnb[] airbnbs, string type, int quantity)
+    {
+      DateTime quickSortStart = DateTime.Now;
+
+      double[] quickSortTimes = new double[REPETITIONS_TIME];
+      for (int repetition = 0; repetition < REPETITIONS_TIME; repetition++)
+      {
+        Airbnb[] airbnbsQuickSort = Airbnb.GetCopy(airbnbs, quantity);
+
+        Stopwatch watch = Stopwatch.StartNew();
+        Airbnb.QuickSort(airbnbsQuickSort);
+        watch.Stop();
+
+        quickSortTimes[repetition] = watch.ElapsedMilliseconds / 1000.0;
+      }
+
+      double quickAverageTime = AverageTime(quickSortTimes);
+      DateTime quickSortEnd = DateTime.Now;
+      string consoleLog = $"{quantity,-10} {type,-15} {QUICK_SORT,-15} ({quickSortStart} - {quickSortEnd})";
+
+      Log(QUICK_SORT,
+          type,
+          quantity,
+          quickAverageTime,
+          Process.GetCurrentProcess().PeakWorkingSet64 / (1024 * 1024));
+
+      ConsoleLog(consoleLog);
+      Console.WriteLine(consoleLog);
+    }
+
+    private static void Create(Airbnb[] airbnbs, string type, int quantity)
+    {
+
+    }
+
+    #endregion
+
+    #region [ Log ]
+
+    private static double AverageTime(double[] times)
+    {
+      double max = times[0], min = times[0];
+      double sum = times[0];
+
+      for (int i = 1; i < times.Length; i++)
+      {
+        if (times[i] > max)
+          max = times[i];
+        else if (times[i] < min)
+          min = times[i];
+        sum += times[i];
+      }
+
+      return (sum - max - min) / ((times.Length - 2) * 1.0);
+    }
+
+    private static void Log(string description,
+                            string tipo,
+                            int quantity,
+                            double time,
+                            long memory)
+    {
+
+      string path = string.Format(FILE_LOG_WRITE_PATH, StartDate.ToString("dd-MM-yyyy HH-MM"));
+      string linha = string.Empty;
+
+      if (!File.Exists(path))
+      {
+        File.Create(path).Close();
+        linha += "Algoritmo; Tipo; Quantidade; Tempo; Memoria\n";
+      }
+
+      linha += $"{description}; {tipo}; {quantity}; {time}; {memory}\n";
+      File.AppendAllText(path, linha);
+    }
+
+    private static void ConsoleLog(string consoleLog)
+    {
+      string path = string.Format(FILE_CONSOLE_LOG_WRITE_PATH, StartDate.ToString("dd-MM-yyyy HH-MM"));
+      if (!File.Exists(path))
+      {
+        File.Create(path).Close();
+      }
+
+      File.AppendAllText(path, consoleLog + "\n");
+    }
+
+    #endregion
+
+    #region [ Read File ]
+
+    private static Airbnb[] ReadAirbnbFile(string path)
+    {
+      string[] lines = File.ReadAllLines(path);
+      Airbnb[] airbnbs = new Airbnb[lines.Length - 1];
+
+      for (int i = 1; i < lines.Length; i++)
+      {
+        string[] line = lines[i].Split(",");
+        int roomId = Convert.ToInt32(line[0]);
+        int hostId = Convert.ToInt32(line[1]);
+        string roomType = line[2];
+        string country = line[3];
+        string city = line[4];
+        string neighborhood = line[5];
+        double review = Convert.ToDouble(line[6]);
+        double overallSatisfaction = Convert.ToDouble(line[7]);
+        double accommodates = Convert.ToDouble(line[8]);
+        double bedrooms = Convert.ToDouble(line[9]);
+        double price = Convert.ToDouble(line[10]);
+        string propertyType = line[11];
+
+        airbnbs[i - 1] = new Airbnb(roomId,
+                                    hostId,
+                                    roomType,
+                                    city,
+                                    country,
+                                    neighborhood,
+                                    review,
+                                    overallSatisfaction,
+                                    accommodates,
+                                    bedrooms,
+                                    price,
+                                    propertyType);
+      }
+
+      return airbnbs;
+    }
+
+    #endregion
+  }
 }
